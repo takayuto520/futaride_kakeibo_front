f6fccdcace7c6b256f5e41e95d4f73cd72841bc3
takayuto520 <takayuto520@gmail.com>
2026-03-14 10:40:00 +0900

    ナビゲーションバーの位置固定

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterView, RouterLink } from "vue-router";
 
 <template>
   <div id="app" class="bg-background text-text-primary">
-    <header class="bg-primary-light text-surface p-4">
+    <header class="fixed top-0 left-0 right-0 z-50 bg-primary-light text-surface p-4">
       <nav class="flex justify-center space-x-4">
         <RouterLink
           to="/"
@@ -86,7 +86,7 @@ import { RouterView, RouterLink } from "vue-router";
         </RouterLink>
       </nav>
     </header>
-    <main class="container mx-auto p-4">
+    <main class="container mx-auto p-4 pt-24">
       <RouterView />
     </main>
   </div>
